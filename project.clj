879c2b5851f689e306f5882db9baf6f14652c4cb
ghostandthemachine <jonrose08@gmail.com>
2015-08-25 11:13:17 -0600

    fix main issue

--- project.clj
+++ project.clj
@@ -7,25 +7,21 @@
   :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring-server "0.4.0"]
                  [cljsjs/react "0.13.3-0"]
                  [reagent "0.5.0"]
                  [reagent-forms "0.5.3"]
                  [reagent-utils "0.1.5"]
-                 [ring "1.3.2"]
-                 [ring/ring-defaults "0.1.5"]
-                 [prone "0.8.2"]
-                 [compojure "1.3.4"]
+
                  [hiccup "1.0.5"]
+                 
                  [environ "1.0.0"]
                  [org.clojure/clojurescript "1.7.48" :scope "provided"]
                  [secretary "1.2.3"]
-                 [com.taoensso/timbre "4.0.2"]
-                 [org.danielsz/system "0.1.8"]
-                 [http-kit "2.1.19"]
+
                  [thinktopic/think.service "0.2.1-SNAPSHOT"]
-                 [metosin/compojure-api "0.22.1"]
-                 [garden "1.2.5"]]
+                 
+                 [garden "1.2.5"]
+                 ]
 
   :plugins [[lein-environ "1.0.0"]
             [lein-asset-minifier "0.2.2"]
@@ -46,8 +42,6 @@
 
   :uberjar-name "raven.jar"
 
-  :main raven.main
-
   :clean-targets ^{:protect false} [:target-path
                                     [:cljsbuild :builds :app :compiler :output-dir]
                                     [:cljsbuild :builds :app :compiler :output-to]]
@@ -66,7 +60,17 @@
   :profiles {:dev {:repl-options {:init-ns user
                                   :nrepl-middleware []}
 
-                   :dependencies [[ring-mock "0.1.5"]
+                   :dependencies [[ring-server "0.4.0"]
+                                  [ring "1.3.2"]
+                                  [ring/ring-defaults "0.1.5"]
+                                  [prone "0.8.2"]
+                                  [compojure "1.3.3"]
+                                  [metosin/compojure-api "0.22.1"]
+                                  
+                                  [com.taoensso/timbre "4.0.2"]
+                                  [org.danielsz/system "0.1.8"]
+                                  [http-kit "2.1.19"]
+                                  [ring-mock "0.1.5"]
                                   [ring/ring-devel "1.3.2"]
                                   [leiningen-core "2.5.1"]
                                   [lein-figwheel "0.3.5"]
