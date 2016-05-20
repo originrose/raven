17c073f3816766ce501b49f55fa79a6f2dd25693
Sean Doig <sean.doig@style.com>
2016-05-20 10:42:35 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/raven "0.1.2-SNAPSHOT"
+(defproject thinktopic/raven "0.1.2"
   :description "A simple notifications library using reagent."
   :url "http://www.github.com/thinktopic/raven"
   :license "Eclipse License"
